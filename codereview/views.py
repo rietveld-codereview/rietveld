851862f65a538572a684a500c8051e91e5c3e473
John Abd-El-Malek <john.abdelmalek@gmail.com>
2009-08-26 17:51:27 +0000

    Make any chromium accounts able to view any private issue

--- codereview/views.py
+++ codereview/views.py
@@ -494,7 +494,8 @@ def _can_view_issue(user, issue):
   return (not issue.private
           or issue.owner == user
           or user_email in issue.cc
-          or user_email in issue.reviewers)
+          or user_email in issue.reviewers
+          or user.email().endswith("@chromium.org"))
 
 
 ### Decorators for request handlers ###
