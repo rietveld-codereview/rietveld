3ee9b9f46a8291afa0089bc501ff4fe7a4d5552b
Andi Albrecht <albrecht.andi@gmail.com>
2009-11-12 15:58:53 +0000

    Defer mail.send_mail() calls [issue 152093].

--- codereview/views.py
+++ codereview/views.py
@@ -38,6 +38,7 @@ from google.appengine.api import users
 from google.appengine.api import urlfetch
 from google.appengine.api import xmpp
 from google.appengine.ext import db
+from google.appengine.ext import deferred
 from google.appengine.ext.db import djangoforms
 from google.appengine.runtime import DeadlineExceededError
 from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
@@ -2475,7 +2476,8 @@ def _make_message(request, issue, message, comments=None, send_mail=False,
     kwds = {}
     if cc:
       kwds['cc'] = [_encode_safely(address) for address in cc]
-    mail.send_mail(sender=my_email,
+    deferred.defer(mail.send_mail,
+                   sender=my_email,
                    to=[_encode_safely(address) for address in to],
                    subject=_encode_safely(subject),
                    body=_encode_safely(body),
