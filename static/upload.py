b240b4e237487eb56e825e6172e43a4868b4c59e
evan.martin <evan.martin@localhost>
2009-12-17 05:10:40 +0000

    Fix rename handling in git uploader when not run from repo root.
    Fixes this bug:
    
    Got error status from ['git', 'show',
    'chromeos_login_manager/emit_login_prompt_ready.cc']:
    
    "git show <pathname>" assumes a path relative to the current working
    directory so it will error if not in the root of the repository.
    Also, show will not actually show the file. To show the file, the
    syntax is:
    
    git show <revision>:<pathname>
    
    With this syntax, the pathname is relative to the root of the repository.
    
    Patch from Mandeep Singh Baines <msb@chromium.org>.

--- static/upload.py
+++ static/upload.py
@@ -1108,7 +1108,7 @@ class GitVCS(VersionControlSystem):
       status = "A +"  # Match svn attribute name for renames.
       if filename not in self.hashes:
         # If a rename doesn't change the content, we never get a hash.
-        base_content = RunShell(["git", "show", filename])
+        base_content = RunShell(["git", "show", "HEAD:" + filename])
     elif not hash_before:
       status = "A"
       base_content = ""
