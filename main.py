6575489efaff34a2db616358dc1527ab8c35e2de
Marc-Antoine Ruel <maruel@chromium.org>
2010-11-26 19:09:41 +0000

    Merge trunk r598 to r614.
    This includes switch to django 1.1.

--- main.py
+++ main.py
@@ -31,15 +31,7 @@ import logging
 logging.info('Loading %s, app version = %s',
              __name__, os.getenv('CURRENT_VERSION_ID'))
 
-# Declare the Django version we need.
-from google.appengine.dist import use_library
-use_library('django', '1.0')
-
-# Fail early if we can't import Django 1.x.  Log identifying information.
-import django
-logging.info('django.__file__ = %r, django.VERSION = %r',
-             django.__file__, django.VERSION)
-assert django.VERSION[0] >= 1, "This Django version is too old"
+import appengine_config
 
 # AppEngine imports.
 from google.appengine.ext.webapp import util
@@ -57,6 +49,10 @@ def BREAKPOINT():
 
 
 # Custom Django configuration.
+# NOTE: For obscure reasons, this must be repeated here as well as in
+# appengine_config.py.  Without the duplication, we get tracebacks
+# from Django complaining about ROOT_URLCONF when the first hit to a
+# fresh process is /_ah/stats, and the second is /.
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from django.conf import settings
 settings._target = None
