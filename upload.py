0f2c939a260267b2929115665fcad2620d72954e
Marc-Antoine Ruel <maruel@chromium.org>
2011-02-08 14:54:05 +0000

    Merged /trunk:r659-660

--- upload.py
+++ upload.py
@@ -93,9 +93,9 @@ VCS_UNKNOWN = "Unknown"
 
 # whitelist for non-binary filetypes which do not start with "text/"
 # .mm (Objective-C) shows up as application/x-freemind on my Linux box.
-TEXT_MIMETYPES = ['application/javascript', 'application/x-javascript',
-                  'application/xml', 'application/x-freemind', 
-                  'application/x-sh']
+TEXT_MIMETYPES = ['application/javascript', 'application/json',
+                  'application/x-javascript', 'application/xml',
+                  'application/x-freemind', 'application/x-sh']
 
 VCS_ABBREVIATIONS = {
   VCS_MERCURIAL.lower(): VCS_MERCURIAL,
