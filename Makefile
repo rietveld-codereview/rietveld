66a6dd4db2f22a2b461108c8efe75f36a810c3b3
anatoly techtonik <techtonik@gmail.com>
2011-09-09 15:24:19 +0300

    Issue 4962059: Remove + sign (indicator of local changes) from Rietveld revision number

---
+++ Makefile
@@ -0,0 +1,52 @@
+# Makefile to simplify some common AppEngine actions.
+# Use 'make help' for a list of commands.
+
+DEV_APPSERVER?= dev_appserver.py
+DEV_APPSERVER_FLAGS?=
+
+APPCFG?= appcfg.py
+APPCFG_FLAGS?=
+
+PYTHON?= python2.5
+COVERAGE?= coverage
+SDK_PATH?=
+
+default: help
+
+help:
+	@echo "Available commands:"
+	@sed -n '/^[a-zA-Z0-9_.]*:/s/:.*//p' <Makefile | sort
+
+serve:
+	$(DEV_APPSERVER) $(DEV_APPSERVER_FLAGS) .
+
+serve_remote:
+	$(DEV_APPSERVER) $(DEV_APPSERVER_FLAGS) --address 0.0.0.0 .
+
+serve_email:
+	$(DEV_APPSERVER) $(DEV_APPSERVER_FLAGS) --enable_sendmail .
+
+serve_remote_email:
+	$(DEV_APPSERVER) $(DEV_APPSERVER_FLAGS) --enable_sendmail --address 0.0.0.0 .
+
+update:
+	@echo "Updating `cat app.yaml | sed -n 's/^application: *//p'`"
+	@echo "`hg id -n`:`hg id -i`" >REVISION
+	$(APPCFG) $(APPCFG_FLAGS) update .
+
+upload: update
+
+deploy: update
+
+update_indexes:
+	$(APPCFG) $(APPCFG_FLAGS) update_indexes .
+
+vacuum_indexes:
+	$(APPCFG) $(APPCFG_FLAGS) vacuum_indexes .
+
+test:
+	$(PYTHON) tests/run_tests.py $(SDK_PATH)
+
+coverage:
+	$(COVERAGE) run --branch tests/run_tests.py $(SDK_PATH)
+	$(COVERAGE) html --include="codereview/*"
---
+++ settings.py
@@ -0,0 +1,60 @@
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Minimal Django settings."""
+
+import os
+
+from google.appengine.api import app_identity
+
+APPEND_SLASH = False
+DEBUG = os.environ['SERVER_SOFTWARE'].startswith('Dev')
+INSTALLED_APPS = (
+    'codereview',
+)
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
+    'codereview.middleware.AddUserToRequestMiddleware',
+)
+ROOT_URLCONF = 'urls'
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+)
+TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates'),
+    )
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    )
+FILE_UPLOAD_HANDLERS = (
+    'django.core.files.uploadhandler.MemoryFileUploadHandler',
+)
+FILE_UPLOAD_MAX_MEMORY_SIZE = 1048576  # 1 MB
+
+MEDIA_URL = '/static/'
+
+appid = app_identity.get_application_id()
+RIETVELD_INCOMING_MAIL_ADDRESS = ('reply@%s.appspotmail.com' % appid)
+RIETVELD_INCOMING_MAIL_MAX_SIZE = 500 * 1024  # 500K
+RIETVELD_REVISION = '<unknown>'
+try:
+    RIETVELD_REVISION = open(
+        os.path.join(os.path.dirname(__file__), 'REVISION')
+    ).read()
+except:
+    pass
+
+UPLOAD_PY_SOURCE = os.path.join(os.path.dirname(__file__), 'upload.py')
