1d1e727b264ce74df86a3e94058af43ceea722b2
Marc-Antoine Ruel <maruel@chromium.org>
2011-10-25 12:34:31 -0400

    Fix W0611: Unused import FOO

--- codereview/feeds.py
+++ codereview/feeds.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datetime
 import md5
 
 from django.contrib.syndication.feeds import Feed
