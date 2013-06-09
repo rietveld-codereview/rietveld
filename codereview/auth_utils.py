f2767f13da787f2c90fae5337c5b75a4738496f4
Andi Albrecht <albrecht.andi@gmail.com>
2013-06-09 07:59:00 +0200

    Simplification of auth_utils since OAuth API now includes Client ID, by dhermes.
    Review: https://codereview.appspot.com/9964048/

--- codereview/auth_utils.py
+++ codereview/auth_utils.py
@@ -36,23 +36,16 @@ See google.appengine.ext.endpoints.users_id_token for reference:
  'appengine/ext/endpoints/users_id_token.py')
 """
 
-import json
 import logging
 import os
-import urllib
 
 from google.appengine.api import oauth
-from google.appengine.api import urlfetch
 from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.ext import ndb
 
 
 EMAIL_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
-TOKENINFO_URL = 'https://www.googleapis.com/oauth2/v1/tokeninfo'
-_ALLOWED_AUTH_SCHEMES = ('OAUTH ', 'BEARER ')
-
-IS_DEV = os.environ['SERVER_SOFTWARE'].startswith('Dev')  # Development server
 
 
 class SecretKey(ndb.Model):
@@ -94,137 +87,6 @@ class SecretKey(ndb.Model):
       return config.client_id, config.client_secret
 
 
-def get_oauth_token_from_env():
-  """Gets an OAuth 2.0 token string from an HTTP header set in the environment.
-
-  Returns:
-    String containing the HTTP Authorization header, if HTTP_AUTHORIZATION is
-        found in the OS environment and if the value begins one of the accept
-        scheme values. Otherwise returns None.
-  """
-  auth_header = os.getenv('HTTP_AUTHORIZATION')
-  if auth_header is None:
-    logging.debug('No authorization header sent with request.')
-    return
-
-  for auth_scheme in _ALLOWED_AUTH_SCHEMES:
-    if auth_header.upper().startswith(auth_scheme):
-      return auth_header[len(auth_scheme):]
-
-  logging.warning('Auth header %r does not begin with any of the allowed '
-                  'schemes: %r', auth_header, _ALLOWED_AUTH_SCHEMES)
-
-
-def get_token_info(token):
-  """Gets OAuth 2.0 token info from Google's token info API.
-
-  Args:
-    token: String containing on OAuth 2.0 token.
-
-  Returns:
-    Dictionary parsed from the JSON in the token info API response. If the
-        response is not a 200, the response body can't be decoded as JSON or
-        it isn't decoded as a dictionary, returns None.
-  """
-  logging.debug('Getting token info for OAuth token: %r:', token)
-
-  tokeninfo_result = urlfetch.fetch(
-      '%s?%s' % (TOKENINFO_URL, urllib.urlencode({'access_token': token})))
-  if tokeninfo_result.status_code != 200:
-    logging.warning('Tokeninfo could not be retrieved. Token info API returned '
-                    'status code %d.', tokeninfo_result.status_code)
-    return
-
-  try:
-    token_info = json.loads(tokeninfo_result.content)
-    if not isinstance(token_info, dict):
-      raise ValueError('Parsed token info not a dictionary.')
-    return token_info
-  except (ValueError, TypeError):
-    logging.warning('Token info JSON couldn\'t be parsed.')
-
-
-def check_token_info(token_info, oauth_user):
-  """Checks that token info agrees with user from OAuth library.
-
-  For the token info to be valid for this application, all the following
-  must hold:
-    - The token info must contain a *verified* email which is equal to the
-      email on the oauth_user object.
-    - The token info must have scope equal to Google's email scope, meaning
-      this scope can only be used to obtain the user's email.
-    - The token info must contain identical audience and issued to values. If
-      they differ, this indicated the token was not minted from a command line
-      tool.
-    - The audience (and issued to) value must be equal to the client ID
-      associated with this application.
-
-  Args:
-    token_info: Dictionary containing token information retrieved from Google's
-        token info API.
-    oauth_user: users.User object that was retrieved from the App Engine OAuth
-        library.
-
-  Returns:
-    Boolean indicating whether or not token info is valid for this application
-        and agrees with the OAuth user.
-  """
-  # Email checks
-  email = token_info.get('email')
-  if email is None:
-    logging.warning('Token info doesn\'t include an email address.')
-    return False
-
-  if not token_info.get('verified_email'):
-    logging.warning('Token info email %r isn\'t verified.', email)
-    return False
-
-  if email != oauth_user.email():
-    logging.warning('Token info email %r disagrees with value returned by '
-                    'OAuth library: %r.', email, oauth_user.email())
-    return False
-
-  # Scope check
-  if 'scope' not in token_info:
-    logging.warning('No scope in token info.')
-    return False
-
-  scope_string = token_info['scope']
-  if not isinstance(scope_string, basestring):
-    logging.warning('Scope not a string.')
-    return False
-
-  scope_list = scope_string.split(' ')
-  # Scopes must be separated by a single space.
-  if '' in scope_list:
-    logging.warning('Scope string %r had unexpected format.', scope_string)
-    return False
-
-  if EMAIL_SCOPE not in scope_list:
-    logging.warning('Scope list %r does not contain email scope.', scope_list)
-    return False
-
-  # Audience checks
-  audience = token_info.get('audience')
-
-  if audience is None:
-    logging.warning('Audience is required and isn\'t specified in token info.')
-    return False
-
-  if audience != token_info.get('issued_to'):
-    logging.warning('Command line tokens must have the same issued to and '
-                    'audience. Audience %r is different from issued to %r.',
-                    audience, token_info.get('issued_to'))
-    return False
-
-  client_id, _ = SecretKey.get_config()
-  if audience != client_id:
-    logging.warning('Audience %r not intended for this application.', audience)
-    return False
-
-  return True
-
-
 def get_current_rietveld_oauth_user():
   """Gets the current OAuth 2.0 user associated with a request.
 
@@ -237,20 +99,20 @@ def get_current_rietveld_oauth_user():
   """
   # TODO(dhermes): Address local environ here as well.
   try:
-    current_oauth_user = oauth.get_current_user(EMAIL_SCOPE)
+    current_client_id = oauth.get_client_id(EMAIL_SCOPE)
   except oauth.Error:
     return
 
-  token = get_oauth_token_from_env()
-  if token is None:
+  accepted_client_id, _ = SecretKey.get_config()
+  if accepted_client_id != current_client_id:
+    logging.warning('Client ID %r not intended for this application.',
+                    current_client_id)
     return
 
-  token_info = get_token_info(token)
-  if token_info is None:
-    return
-
-  if check_token_info(token_info, current_oauth_user):
-    return current_oauth_user
+  try:
+    return oauth.get_current_user(EMAIL_SCOPE)
+  except oauth.Error:
+    logging.warning('A Client ID was retrieved with no corresponsing user.')
 
 
 def get_current_user():
--- tests/test_auth_utils.py
+++ tests/test_auth_utils.py
@@ -18,49 +18,32 @@ import urllib
 
 from google.appengine.api import oauth
 from google.appengine.api import users
-from google.appengine.api.user_service_stub import _OAUTH_EMAIL as OAUTH_EMAIL
-from google.appengine.api import urlfetch
+from google.appengine.api.user_service_stub import _OAUTH_CLIENT_ID
 
 from utils import TestCase
 
 from codereview import auth_utils
 
 
-TOKENINFO_URL_WITH_QUERY = auth_utils.TOKENINFO_URL + '?access_token='
-DEFAULT_STATUS_CODE = 200
 TEST_EMAIL = 'foo@example.com'
 EMAIL_SCOPE = auth_utils.EMAIL_SCOPE
 CLIENT_ID = 'dummy29.apps.googleusercontent.com'
 
-
-class DummyURLFetchResponse(object):
-  def __init__(self, content, status_code=DEFAULT_STATUS_CODE):
-    self.content = content
-    self.status_code = status_code
 
 
 class TestAuthUtils(TestCase):
 
   def setUp(self):
     super(TestAuthUtils, self).setUp()
-    self.testbed.init_urlfetch_stub()
     # User service stub used in TestAuthUtils, this protobuf service
     # includes the OAuth API.
     self.oauth_login(TEST_EMAIL)
-    self.auth_header = None
-    if 'HTTP_AUTHORIZATION' in os.environ:
-      self.auth_header = os.environ['HTTP_AUTHORIZATION']
-      del os.environ['HTTP_AUTHORIZATION']
-    self.original_fetch = urlfetch.fetch
 
     auth_utils.SecretKey.set_config(CLIENT_ID, 'dummy.secret')
 
   def tearDown(self):
     super(TestAuthUtils, self).tearDown()
     self.oauth_logout()
-    if self.auth_header is not None:
-      os.environ['HTTP_AUTHORIZATION'] = self.auth_header
-    urlfetch.fetch = self.original_fetch
 
   def cookie_login(self, email, is_admin=False):
     """Logs in Cookie user identified by email."""
@@ -72,12 +55,13 @@ class TestAuthUtils(TestCase):
     self.logout()
     os.environ['USER_IS_ADMIN'] = '0'
 
-  def oauth_login(self, email, is_admin=False):
+  def oauth_login(self, email, is_admin=False, client_id=CLIENT_ID):
     """Logs in OAuth user identified by email."""
     stub_map = self.testbed._test_stub_map._APIProxyStubMap__stub_map
     user_stub = stub_map['user']
     user_stub._UserServiceStub__email = email
     user_stub._UserServiceStub__is_admin = is_admin
+    user_stub._client_id = client_id
 
   def oauth_logout(self):
     """Logs the OAuth user out."""
@@ -85,30 +69,7 @@ class TestAuthUtils(TestCase):
     user_stub = stub_map['user']
     user_stub._UserServiceStub__email = None
     user_stub._UserServiceStub__is_admin = False
-
-  def make_tokeninfo_fetch(self, response, status_code=DEFAULT_STATUS_CODE):
-    """Creates a dummy replacement for urlfetch.fetch.
-
-    This is intended to be used only for token info fetches and fails
-    if the URL is not a token info URL or if any arguments other than
-    the URL are passed in.
-
-    Args:
-      response: The response to be returned when this method is called.
-      status_code: The status code of the response.
-
-    Returns:
-      Dummy method for fetching token info.
-    """
-    def dummy_fetch(url, *args, **kwargs):
-      # Only allow the signature used in auth utils
-      if not url.startswith(TOKENINFO_URL_WITH_QUERY):
-        self.fail('Token info fetcher called with non-tokeninfo '
-                  'URL: %r.' % (url,))
-      if args or kwargs:
-        self.fail('Only the URL can be specified in token info fetcher.')
-      return DummyURLFetchResponse(response, status_code=status_code)
-    return dummy_fetch
+    user_stub._client_id = _OAUTH_CLIENT_ID
 
   def test_oauth_works_as_expected_in_test(self):
     oauth_user = oauth.get_current_user(EMAIL_SCOPE)
@@ -116,169 +77,18 @@ class TestAuthUtils(TestCase):
     self.assertEqual(oauth_user.auth_domain(), 'gmail.com')
     self.assertEqual(oauth_user.user_id(), '0')
 
-  def test_get_oauth_token_from_env_no_header(self):
-    # Set-up guarentees the header won't be set
-    self.assertIsNone(auth_utils.get_oauth_token_from_env())
-
-  def test_get_oauth_token_from_env_bad_header(self):
-    os.environ['HTTP_AUTHORIZATION'] = 'Not close'
-    self.assertIsNone(auth_utils.get_oauth_token_from_env())
-    os.environ['HTTP_AUTHORIZATION'] = 'OAuthSoCloseButNoSpace'
-    self.assertIsNone(auth_utils.get_oauth_token_from_env())
-
-  def test_get_oauth_token_from_env_success(self):
-    token = 'abc'
-
-    os.environ['HTTP_AUTHORIZATION'] = 'OAuth ' + token
-    self.assertEqual(auth_utils.get_oauth_token_from_env(), token)
-
-    os.environ['HTTP_AUTHORIZATION'] = 'Bearer ' + token
-    self.assertEqual(auth_utils.get_oauth_token_from_env(), token)
-
-    os.environ['HTTP_AUTHORIZATION'] = 'oaUTh ' + token  # Case doesn't matter
-    self.assertEqual(auth_utils.get_oauth_token_from_env(), token)
-
-  def test_get_token_info_bad_status(self):
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch('Response is Moot',
-                                               status_code=400)
-    self.assertIsNone(auth_utils.get_token_info('abc'))
-
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch('Response is Moot',
-                                               status_code=503)
-    self.assertIsNone(auth_utils.get_token_info('abc'))
-
-  def test_get_token_info_bad_content(self):
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch(None)  # Wrong type
-    self.assertIsNone(auth_utils.get_token_info('abc'))
-
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch('{')  # Invalid JSON
-    self.assertIsNone(auth_utils.get_token_info('abc'))
-
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch('[]')  # Invalid return type
-    self.assertIsNone(auth_utils.get_token_info('abc'))
-
-  def test_get_token_info_success(self):
-    token_info = {'email': TEST_EMAIL}
-    token_info_json = json.dumps(token_info)
-
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch(token_info_json)
-    self.assertEqual(auth_utils.get_token_info('abc'), token_info)
-
-  def test_check_token_info_invalid_email(self):
-    # Auth domain should be set by users stub, which is registered in TestCase
-    oauth_user = users.User(TEST_EMAIL)
-    token_info = {}
-
-    # No email
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-
-    # Email not verified
-    token_info['email'] = TEST_EMAIL
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-    token_info['verified_email'] = False
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-
-    token_info['verified_email'] = True
-
-    # Email disagrees with OAuth user
-    token_info['email'] = 'foo'
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-    token_info['email'] = TEST_EMAIL
-
-    # No scope at all
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-    # Scope not equal to email
-    token_info['scope'] = 'fake scope right here'
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-
-  def test_check_token_info_invalid_audience(self):
-    # Auth domain should be set by users stub, which is registered in TestCase
-    oauth_user = users.User(TEST_EMAIL)
-    token_info = {
-      'email': TEST_EMAIL,
-      'verified_email': True,
-      'scope': EMAIL_SCOPE
-    }
-
-    # No audience
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-
-    wrong_client_id = 'foo'
-    token_info['audience'] = wrong_client_id
-
-    # No issued to
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-    # Issued to differs from audience
-    token_info['issued_to'] = 'bar'
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-
-    # Issued to and audience agree, audience not equal to CLIENT_ID
-    token_info['issued_to'] = wrong_client_id
-    self.assertFalse(auth_utils.check_token_info(token_info, oauth_user))
-
-  def test_check_token_info_success(self):
-    # Auth domain should be set by users stub, which is registered in TestCase
-    oauth_user = users.User(TEST_EMAIL)
-    token_info = {
-      'email': TEST_EMAIL,
-      'verified_email': True,
-      'scope': EMAIL_SCOPE,
-      'audience': CLIENT_ID,
-      'issued_to': CLIENT_ID,
-    }
-    self.assertTrue(auth_utils.check_token_info(token_info, oauth_user))
-
-  def test_get_current_rietveld_oauth_user_bad_token(self):
-    os.environ['HTTP_AUTHORIZATION'] = 'Not close'
-    self.assertIsNone(auth_utils.get_current_rietveld_oauth_user())
-    os.environ['HTTP_AUTHORIZATION'] = 'OAuthSoCloseButNoSpace'
-    self.assertIsNone(auth_utils.get_current_rietveld_oauth_user())
-
-  def test_get_current_rietveld_oauth_user_bad_token_info(self):
-    os.environ['HTTP_AUTHORIZATION'] = 'Bearer Grylls'
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch('Response is Moot',
-                                               status_code=400)
-    self.assertIsNone(auth_utils.get_current_rietveld_oauth_user())
-
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch('{')  # Invalid JSON
-    self.assertIsNone(auth_utils.get_current_rietveld_oauth_user())
-
-    # tearDown will set urlfetch.fetch back to the original for us
-    token_info_no_audience = {
-      'email': TEST_EMAIL,
-      'verified_email': True,
-      'scope': EMAIL_SCOPE,
-    }
-    urlfetch.fetch = self.make_tokeninfo_fetch(
-        json.dumps(token_info_no_audience))
-    self.assertIsNone(auth_utils.get_current_rietveld_oauth_user())
-
-  def set_rietveld_user_success_vars(self, email=TEST_EMAIL):
-    os.environ['HTTP_AUTHORIZATION'] = 'Bearer Grylls'
-    token_info = json.dumps({
-      'email': email,
-      'verified_email': True,
-      'scope': EMAIL_SCOPE,
-      'audience': CLIENT_ID,
-      'issued_to': CLIENT_ID,
-    })
-    # tearDown will set urlfetch.fetch back to the original for us
-    urlfetch.fetch = self.make_tokeninfo_fetch(token_info)
+    client_id = oauth.get_client_id(EMAIL_SCOPE)
+    self.assertEqual(client_id, CLIENT_ID)
 
   def test_get_current_rietveld_oauth_user_success(self):
-    self.set_rietveld_user_success_vars()
     oauth_user = auth_utils.get_current_rietveld_oauth_user()
     self.assertEqual(oauth_user.email(), TEST_EMAIL)
     self.assertEqual(oauth_user.auth_domain(), 'gmail.com')
     self.assertEqual(oauth_user.user_id(), '0')
+
+  def test_get_current_rietveld_oauth_bad_client_id(self):
+    self.oauth_login('any@mail.com', client_id='bad.id')
+    self.assertIsNone(auth_utils.get_current_rietveld_oauth_user())
 
   def test_get_current_user_no_users(self):
     self.cookie_logout()
@@ -296,7 +106,6 @@ class TestAuthUtils(TestCase):
   def test_get_current_user_success(self):
     oauth_mail = 'oauth@mail.com'
     self.oauth_login(oauth_mail)
-    self.set_rietveld_user_success_vars(email=oauth_mail)
 
     current_user = auth_utils.get_current_user()
     self.assertEqual(current_user.email(), oauth_mail)
@@ -306,7 +115,6 @@ class TestAuthUtils(TestCase):
     oauth_mail = 'oauth@mail.com'
     self.cookie_login(cookie_mail)
     self.oauth_login(oauth_mail)
-    self.set_rietveld_user_success_vars(email=oauth_mail)
 
     # Make sure the OAuth API still works
     oauth_user = oauth.get_current_user(EMAIL_SCOPE)
@@ -342,7 +150,6 @@ class TestAuthUtils(TestCase):
     # Can't call is_current_user_admin twice in the same test because the OAuth
     # user gets set once and cached.
     oauth_mail = 'oauth@mail.com'
-    self.set_rietveld_user_success_vars(email=oauth_mail)
 
     # Not an admin
     self.oauth_login(oauth_mail, is_admin=False)
@@ -352,7 +159,6 @@ class TestAuthUtils(TestCase):
     # Can't call is_current_user_admin twice in the same test because the OAuth
     # user gets set once and cached.
     oauth_mail = 'oauth@mail.com'
-    self.set_rietveld_user_success_vars(email=oauth_mail)
 
     # Is an admin
     self.oauth_login(oauth_mail, is_admin=True)
