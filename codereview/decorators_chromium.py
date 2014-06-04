66f6b9aeb3b3ed6b6a57778070e5b6881231f6b9
Jason Robbins <jrobbins@chromium.org>
2014-06-04 12:19:52 -0700

    Fix missing import, and key vs. entity error.
    Review URL: https://codereview.appspot.com/103090044/

--- codereview/decorators_chromium.py
+++ codereview/decorators_chromium.py
@@ -36,20 +36,20 @@ def binary_required(func):
   @deco.patch_required
   def binary_wrapper(request, content_type, *args, **kwds):
     if content_type == "0":
-      content = request.patch.content
+      content_key = request.patch.content
     elif content_type == "1":
-      content = request.patch.patched_content
-      if not content or not content.data:
+      content_key = request.patch.patched_content
+      if not content_key or not content_key.get().data:
         # The file was not modified. It was likely moved without modification.
         # Return the original file.
-        content = request.patch.content
+        content_key = request.patch.content
     else:
       # Other values are erroneous so request.content won't be set.
       return responses.HttpTextResponse(
           'Invalid content type: %s, expected 0 or 1' % content_type,
           status=404)
     request.mime_type = mimetypes.guess_type(request.patch.filename)[0]
-    request.content = content
+    request.content = content_key.get()
     return func(request, *args, **kwds)
 
   return binary_wrapper
