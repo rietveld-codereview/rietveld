05b5aff391654e0897d12c06f026cfcf93a9a851
Jason Robbins <jrobbins@chromium.org>
2014-08-07 16:40:27 -0700

    Add help text to search date fields.
    Review URL: https://codereview.appspot.com/125850043/

--- codereview/views.py
+++ codereview/views.py
@@ -463,7 +463,9 @@ class SearchForm(forms.Form):
                               label="Repository ID")
   base = forms.CharField(required=False, max_length=MAX_URL)
   private = forms.NullBooleanField(required=False)
-  created_before = forms.DateTimeField(required=False, label='Created before')
+  created_before = forms.DateTimeField(
+    required=False, label='Created before',
+    help_text='Format: YYYY-MM-DD and optional: hh:mm:ss')
   created_after = forms.DateTimeField(
       required=False, label='Created on or after')
   modified_before = forms.DateTimeField(required=False, label='Modified before')
